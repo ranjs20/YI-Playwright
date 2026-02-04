ac2aad03d15d0510eb7a57542c77390f3c383c07
Ranjita Kharel <ranjitakjn@gmail.com>
2026-02-04 11:41:19 +0545

    Right Click Action

--- tests/MouseActions/rightClick.spec.js
+++ tests/MouseActions/rightClick.spec.js
@@ -1,8 +1,10 @@
-import {test,expect} from '@playwright/test'
-test("Right Click",async({page})=>{
-    await page.goto("https://the-internet.herokuapp.com/context_menu")
-    const button =await page.locator('#hot-spot')
-    //For right click
-    await button.click({button:'right'});
+import { test, expect } from '@playwright/test';
+
+test("Right Click Practice", async ({ page }) => {
+    await page.goto("https://swisnl.github.io/jQuery-contextMenu/demo.html")
+    const button=await page.locator('//span[normalize-space()="right click me"]')
+    await button.click({button: 'right'});
     await page.waitForTimeout(5000);
-})
+
+   
+});
